75912f6f2ac645394804c0cf90a4acbdea26fca6
Gustavo <gusta99sant@gmail.com>
2021-03-19 21:55:41 -0300

    chore: Project setup

---
+++ src/pages/index.tsx
@@ -0,0 +1,10 @@
+import GlobalStyle from '../styles/global';
+
+export default function Home() {
+  return (
+    <>
+      <h1>Hello World!</h1>
+      <GlobalStyle />
+    </>
+  )
+}
---
+++ src/styles/global.ts
@@ -0,0 +1,45 @@
+import { createGlobalStyle } from 'styled-components';
+
+export default createGlobalStyle`
+
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+@media(max-width: 1080px) {
+  html {
+    font-size: 93.75%; 
+  }
+}
+
+@media(max-width: 720px) {
+  html {
+    font-size: 87.5%; 
+  }
+}
+
+body {
+  background: var(--background);
+  color: var(--text);
+}
+
+body, button {
+  font: 400 1rem Roboto, sans-serif;
+}
+
+button {
+  cursor: pointer;
+}
+
+a {
+  color: inherit;
+  text-decoration: none;
+}
+
+ul {
+  list-style-type: none;
+}
+
+`;
