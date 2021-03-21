0af112dd7aee088dcd8eedb62a245e7a25bfd99b
Gustavo <gusta99sant@gmail.com>
2021-03-21 17:29:45 -0300

    fix: GlobalStyle reference

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,9 @@
-import GlobalStyle from '../styles/global';
-import CharacterDetail from './CharacterDetail';
 import CharactersList from './CharactersList';
 
 export default function Home() {
   return (
     <>
       <CharactersList />
-      <GlobalStyle />
     </>
   )
 }
