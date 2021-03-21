df4a428fe0ad2ebdaad579dd38254a9b34dae24f
Gustavo <gusta99sant@gmail.com>
2021-03-21 00:06:28 -0300

    style: Hover transtion in character card

--- src/components/modules/CharacterCard/styles.ts
+++ src/components/modules/CharacterCard/styles.ts
@@ -1,7 +1,7 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
-  grid-column: span 4 ;
+  grid-column: span 3 ;
 
   display: flex;
   flex-direction: column;
@@ -13,11 +13,27 @@ export const Content = styled.div`
     img {
       border: 4px solid #0A0F0D;
       width: 100%;
+
+      transition: transform 0.4s;
     }
   }
 
   h3 {
     text-align: center;
     margin: 0.7rem 0 0.5rem 0;
+    color: #0A0F0D;
+
+    transition: color 0.4s;
+  }
+
+  &:hover {
+    img {
+      transform: scale(1.05);
+
+    }
+
+    h3 {
+      color: #EC1D24;
+    }
   }
 `;
