e9b1e77c21b06d72662477d878f5be04c2d99162
Gustavo <gusta99sant@gmail.com>
2021-03-20 21:26:23 -0300

    chore: Create Hero component

---
+++ src/components/modules/Hero/index.tsx
@@ -0,0 +1,20 @@
+import { Container, Content, Context } from './styles';
+
+export default function Hero() {
+  return (
+    <Container>
+      <Content>
+        <div className="pop"></div>
+        <div className="hero-mask"></div>
+        <div>
+          {/* <img src="/assets/images/hero-banner.jpeg" alt="Banner MARVEL" /> */}
+        </div>
+      </Content>
+
+      <Context>
+        <h2>MARVEL BOOK</h2>
+        <p>ALGUM TEXTINHO DA HORA AQUI</p>
+      </Context>
+    </Container>
+  );
+}
---
+++ src/components/modules/Hero/styles.ts
@@ -0,0 +1,30 @@
+import styled from 'styled-components';
+
+export const Container = styled.section`
+  background: #0A0F0D;
+  height: 265px;
+  box-shadow: 0 5px 20px rgba(0, 0, 0, 0.3);
+
+  margin-bottom: 30px;
+`;
+
+export const Content = styled.div``;
+
+export const Context = styled.div`
+  text-align: center;
+  font-style: italic;
+  color: #F9F9F9;
+  height: 100%;
+  
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  p {
+    background: #EC1D24;
+    display: inline;
+    font-weight: 600;
+    padding: 0.3rem 0.5rem;
+  }
+`;
