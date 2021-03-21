3d32bfe4593fb63f56ef49417385029d343f8871
Gustavo <gusta99sant@gmail.com>
2021-03-21 05:35:18 -0300

    feat: Character list pagination

--- src/pages/CharactersList/index.tsx
+++ src/pages/CharactersList/index.tsx
@@ -8,32 +8,46 @@ import SectionTitle from '../../components/modules/SectionTitle';
 import CharacterCard from '../../components/modules/CharacterCard';
 import Spinner from '../../components/elements/Spinner';
 
-import { Container } from './styles';
+import { Container } from '../../styles/CharactersList.styles';
 import { Grid } from '../../styles/grid';
 import Footer from '../../components/modules/Footer';
+import Pagination from '../../components/modules/Pagination';
 
-const apiParams = {
-  "tsKeyHash": "ts=1616179034&apikey=c22ba519d36ef5d64b7c341b94e6c7f9&hash=8a8b24fa59c32ba7bb0920b0c2721d86"
+const LIMIT = 20;
+
+interface Characters {
+  id: number;
+  name: string;
+  thumbnail: {
+    path: string;
+    extension: string;
+  }
+}
+
+interface RequestInfoPagination {
+  total: number;
 }
 
 export default function CharactersList() {
-
-  const [characters, setCharacters] = useState([]);
+  const [characters, setCharacters] = useState<Characters[]>([]);
+  const [requestInfo, setRequestInfo] = useState<RequestInfoPagination>();
   const [hasSpinner, setHasSpinner] = useState(false);
+  const [offset, setOffset] = useState(0);
 
   useEffect(() => {
 
     setHasSpinner(true);
 
-    api.get(`characters?${apiParams.tsKeyHash}`).then((response) => {
-      console.log(response.data.data.results);
+    api.get(`characters?limit=${LIMIT}&offset=${offset}&${apiParams.tsKeyHash}`).then((response) => {
+      setRequestInfo(response.data.data);
       setCharacters(response.data.data.results);
       setHasSpinner(false);
-    }).catch(() => {
+    }).catch((error) => {
       setHasSpinner(true);
+      console.error(error);
     })
 
-  }, []);
+  }, [offset]);
 
   return (
     <>
@@ -47,10 +61,19 @@ export default function CharactersList() {
             <Grid>
               {
                 characters.length > 0 ? characters.map(character => (
-                  <CharacterCard name={character.name} image={`${character.thumbnail.path}/standard_fantastic.${character.thumbnail.extension}`} />
+                  <CharacterCard id={character.id} name={character.name} image={`${character.thumbnail.path}/standard_fantastic.${character.thumbnail.extension}`} />
                 )) : <img src="/assets/images/"></img>
               }
             </Grid>
+
+            {requestInfo && (
+              <Pagination
+                limit={LIMIT}
+                total={requestInfo.total}
+                offset={offset}
+                setOffset={setOffset}
+              />
+            )}
           </>
         }
       </Container>
