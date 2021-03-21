021966560bb2ac9237e954107a998aa34df71645
Gustavo <gusta99sant@gmail.com>
2021-03-20 21:28:50 -0300

    chore: Create Footer component

---
+++ src/components/modules/Footer/index.tsx
@@ -0,0 +1,11 @@
+import { Container, Content } from './styles';
+
+export default function Footer() {
+  return (
+    <Container>
+      <Content>
+        <p>Gustavo Santos</p>
+      </Content>
+    </Container>
+  );
+}
---
+++ src/components/modules/Footer/styles.ts
@@ -0,0 +1,20 @@
+import styled from 'styled-components';
+
+export const Container = styled.footer`
+  background-color: #0A0F0D;
+  color: #F9F9F9;
+  height: 25px;
+  margin-top: 60px;
+
+  display: flex;
+  align-items: center;
+`;
+
+export const Content = styled.div`
+  width: 992px;
+  margin: 0 auto;
+  
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
